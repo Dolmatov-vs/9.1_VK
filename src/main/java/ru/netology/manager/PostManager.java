eea9394cf1f135755dae2065ea6a15348c475e84
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-06-07 13:38:22 +0300

    add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostInfo;
+
+public class PostManager {
+    private PostInfo[] posts;
+
+    public PostInfo[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+//        add logic
+        return null;
+    }
+
+
+    public void delete(int ownerId, int postId){
+//        add logic
+    }
+}
